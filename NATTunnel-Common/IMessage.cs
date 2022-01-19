1e524268b69a6a568650ff6fbcfd63d57f21f634
Lojemiru <33508026+Lojemiru@users.noreply.github.com>
2022-01-19 03:57:04 -0600

    Comment IMessage and PrintConsole

--- NATTunnel-Common/IMessage.cs
+++ NATTunnel-Common/IMessage.cs
@@ -2,9 +2,20 @@
 
 namespace NATTunnel.Common
 {
+    /// <summary>
+    /// Interface for various message types.
+    /// </summary>
     public interface IMessage
     {
+        /// <summary>
+        /// Sends all data from this object to the given <paramref name="writer"/>.
+        /// </summary>
+        /// <param name="writer">The <see cref="BinaryWriter"/> to send data to.</param>
         void Serialize(BinaryWriter writer);
+        /// <summary>
+        /// Reads all data from the given <paramref name="reader"/> into this object.
+        /// </summary>
+        /// <param name="reader">The <see cref="BinaryReader"/> to read data from.</param>
         void Deserialize(BinaryReader reader);
     }
 }
--- NATTunnel-Common/Messages/PrintConsole.cs
+++ NATTunnel-Common/Messages/PrintConsole.cs
@@ -2,11 +2,16 @@ using System.IO;
 
 namespace NATTunnel.Common.Messages
 {
+    /// <summary>
+    /// Class for encapsulating messages between tunnel clients.
+    /// </summary>
     [MessageTypeAttribute(MessageType.MASTER_PRINT_CONSOLE)]
     public class PrintConsole : NodeMessage
     {
+        /// <summary>
+        /// The message to be sent/received.
+        /// </summary>
         public string message;
-
         public override void Serialize(BinaryWriter writer)
         {
             writer.Write(Id);
