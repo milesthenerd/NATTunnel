f7498e78438c3926bb808983b67c8c811d7290b8
milesthenerd <lassiterm@netspec.us>
2024-10-19 19:23:32 -0500

    Fix arp command parsing on Linux

--- NATTunnel/FrameCapture.cs
+++ NATTunnel/FrameCapture.cs
@@ -11,6 +11,7 @@ using System.Text.RegularExpressions;
 using PacketDotNet.Utils;
 using PacketDotNet.Tcp;
 using System.Text;
+using System.Runtime.InteropServices;
 
 namespace NATTunnel;
 
@@ -609,24 +610,49 @@ public class FrameCapture
 
     public IEnumerable<MacIpPair> GetMacIpPairs()
     {
-        System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
-        pProcess.StartInfo.FileName = "arp";
-        pProcess.StartInfo.Arguments = "-a ";
-        pProcess.StartInfo.UseShellExecute = false;
-        pProcess.StartInfo.RedirectStandardOutput = true;
-        pProcess.StartInfo.CreateNoWindow = true;
-        pProcess.Start();
-
-        string cmdOutput = pProcess.StandardOutput.ReadToEnd();
-        string pattern = @"(?<ip>([0-9]{1,3}\.?){4})\s*(?<mac>([a-f0-9]{2}-?){6})";
-
-        foreach (Match m in Regex.Matches(cmdOutput, pattern, RegexOptions.IgnoreCase))
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
-            yield return new MacIpPair()
+            System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
+            pProcess.StartInfo.FileName = "arp";
+            pProcess.StartInfo.Arguments = "-a";
+            pProcess.StartInfo.UseShellExecute = false;
+            pProcess.StartInfo.RedirectStandardOutput = true;
+            pProcess.StartInfo.CreateNoWindow = true;
+            pProcess.Start();
+
+            string cmdOutput = pProcess.StandardOutput.ReadToEnd();
+            string pattern = @"(?<ip>([0-9]{1,3}\.?){4})\s*(?<mac>([a-f0-9]{2}-?){6})";
+
+            foreach (Match m in Regex.Matches(cmdOutput, pattern, RegexOptions.IgnoreCase))
             {
-                MacAddress = m.Groups["mac"].Value,
-                IpAddress = m.Groups["ip"].Value
-            };
+                yield return new MacIpPair()
+                {
+                    MacAddress = m.Groups["mac"].Value,
+                    IpAddress = m.Groups["ip"].Value
+                };
+            }
+        }
+        else
+        {
+            System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
+            pProcess.StartInfo.FileName = "bash";
+            pProcess.StartInfo.Arguments = @"-c ""awk '{print $1, $3}' <(arp -n)""";
+            pProcess.StartInfo.UseShellExecute = false;
+            pProcess.StartInfo.RedirectStandardOutput = true;
+            pProcess.StartInfo.CreateNoWindow = true;
+            pProcess.Start();
+
+            string cmdOutput = pProcess.StandardOutput.ReadToEnd();
+            string pattern = @"(?<ip>([0-9]{1,3}\.?){4})\s*(?<mac>([a-f0-9]{2}:?){6})";
+
+            foreach (Match m in Regex.Matches(cmdOutput, pattern, RegexOptions.IgnoreCase))
+            {
+                yield return new MacIpPair()
+                {
+                    MacAddress = m.Groups["mac"].Value,
+                    IpAddress = m.Groups["ip"].Value
+                };
+            }
         }
     }
 }
--- NATTunnel/Program.cs
+++ NATTunnel/Program.cs
@@ -2,7 +2,7 @@
 
 namespace NATTunnel;
 
-internal static class Program
+public static class Program
 {
     public static void Main()
     {
